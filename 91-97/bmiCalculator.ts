0e25a54e214d558dd818ea98e67e0cd4623f6c3c
zahgenya <zahgenya@gmail.com>
2023-11-27 15:54:46 +0200

    refactored bmiCalculator getting arguments from process arguments

--- 91-97/bmiCalculator.ts
+++ 91-97/bmiCalculator.ts
@@ -1,15 +1,42 @@
-const calculateBmi = (h: number, w: number): string => {
-  let hM = h / 100
-  const result = w / (hM ** 2)
-
-  if (result >= 18.5 && result <= 24.9) {
-    return "Normal (healthy weight)"
-  } else if (result <= 18.5) {
-    return "Bad (underweight)"
-  } else {
-    return "Bad (obese)"
-  }
+interface bmiValues {
+  value1: number;
+  value2: number;
 }
 
+const parseArguments = (args: string[]): bmiValues => {
+  if (args.length < 4) throw new Error("Not enough arguments");
+  if (args.length > 4) throw new Error("Too many arguments");
 
-console.log(calculateBmi(165, 65))
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[2]),
+      value2: Number(args[3]),
+    };
+  } else {
+    throw new Error("Provided arguments were not numbers!");
+  }
+};
+
+const calculateBmi = (h: number, w: number) => {
+  let hM = h / 100;
+  const result = w / hM ** 2;
+
+  if (result >= 18.5 && result <= 24.9) {
+    console.log("Normal (healthy weight)");
+  } else if (result <= 18.5) {
+    console.log("Bad (underweight)");
+  } else {
+    console.log("Bad (obese)");
+  }
+};
+
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  calculateBmi(value1, value2);
+} catch (error: unknown) {
+  let errorMessage = "Something went wrong";
+  if (error instanceof Error) {
+    errorMessage += " Error: " + error.message;
+  }
+  console.log(errorMessage);
+}
