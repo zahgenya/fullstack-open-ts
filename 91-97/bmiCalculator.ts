280b6120693e5e14e1ee125b91d92fb5e52ff5b6
zahgenya <zahgenya@gmail.com>
2023-11-29 15:06:53 +0200

    bmi is now a web application, its getting arguments by query string params

--- 91-97/bmiCalculator.ts
+++ 91-97/bmiCalculator.ts
@@ -1,42 +1,12 @@
-interface bmiValues {
-  value1: number;
-  value2: number;
-}
-
-const parseBmiArguments = (args: string[]): bmiValues => {
-  if (args.length < 4) throw new Error("Not enough arguments");
-  if (args.length > 4) throw new Error("Too many arguments");
-
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      value1: Number(args[2]),
-      value2: Number(args[3]),
-    };
-  } else {
-    throw new Error("Provided arguments were not numbers!");
-  }
-};
-
-const calculateBmi = (h: number, w: number) => {
+export const calculateBmi = (h: number, w: number) => {
   let hM = h / 100;
   const result = w / hM ** 2;
 
   if (result >= 18.5 && result <= 24.9) {
-    console.log("Normal (healthy weight)");
+    return { weight: w, height: h, bmi: "Normal (healthy weight)" };
   } else if (result <= 18.5) {
-    console.log("Bad (underweight)");
+    return { weight: w, height: h, bmi: "Bad (underweight)" };
   } else {
-    console.log("Bad (obese)");
+    return { weight: w, height: h, bmi: "Bad (obese)" };
   }
 };
-
-try {
-  const { value1, value2 } = parseBmiArguments(process.argv);
-  calculateBmi(value1, value2);
-} catch (error: unknown) {
-  let errorMessage = "Something went wrong";
-  if (error instanceof Error) {
-    errorMessage += " Error: " + error.message;
-  }
-  console.log(errorMessage);
-}
--- 91-97/index.ts
+++ 91-97/index.ts
@@ -1,11 +1,26 @@
 import express from 'express';
+import {calculateBmi} from './bmiCalculator';
+
 const app = express();
 
 app.get('/hello', (req, res) => {
   res.send('Hello Full Stack!')
 })
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height)
+  const weight = Number(req.query.weight)
+
+  if (isNaN(height) || isNaN(weight)) {
+    return res.status(400).json({ error: 'malformatted parameters ' })
+  }
+
+  const bmiResult = calculateBmi(height, weight)
+
+  return res.json(bmiResult)
+})
+
+const PORT: number = 3003;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
